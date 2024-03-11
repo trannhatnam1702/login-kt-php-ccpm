edba9b2479907e4312cf7727b99ed76d478031b4
thinh <caodangthinh1@gmail.com>
2024-03-11 14:57:49 +0700

    add label username textbox

--- login.php
+++ login.php
@@ -8,6 +8,20 @@
 </head>
 <body>
     
+<div class="d-flex align-items-center justify-content-center" style="height: 100vh;">
+        <div class="card" style="width: 20rem;">
+            <div class="card-body">
+                <h5>Đăng nhập</h5>
+                <form action="result.php" method="post">
+                    <div class="form-group">
+                        <label><b>Username</b></label>
+                        <input type="text" name="txtUsername" class="form-control" required>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
